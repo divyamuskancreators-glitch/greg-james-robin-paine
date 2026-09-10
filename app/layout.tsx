424088bad7ef4316580d9bcb428cb66c32d83096
martganan24 <mart.ganan24@gmail.com>
2026-09-10 20:08:34 +0800

    Update site description

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export const metadata: Metadata = {
   description: "IOT AUTOMATION SCANDAL",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html
       lang="en"
